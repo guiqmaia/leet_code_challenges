2e2bc89580ccc554458341614aee448e04885185
guiqmaia <guilherme.q.maia@gmail.com>
2022-10-31 18:41:04 -0300

    feat: two sum challenge

---
+++ easy/two_sum.dart
@@ -0,0 +1,17 @@
+void main() {
+  List<int>? result = twoSum([3, 2, 4], 6);
+
+  print(result);
+}
+
+List<int>? twoSum(List<int> nums, int target) {
+  for (int i = 0; i < nums.length; i++) {
+    for (int index = i + 1; index < nums.length; index++) {
+      if (nums[i] + nums[index] == target) {
+        return [i, index];
+      }
+    }
+  }
+
+  return null;
+}
